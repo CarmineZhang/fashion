cae81cbba19a0e6fc546eb2824af043245ab65fc
zhangmin <157507254@qq.com>
2017-08-10 16:05:49 +0800

    initial

---
+++ .postcssrc.js
@@ -0,0 +1,12 @@
+// https://github.com/michael-ciniawsky/postcss-load-config
+
+module.exports = {
+  "plugins": {
+    // to edit target browsers: use "browserslist" field in package.json
+    "autoprefixer": {
+      "browserslist": [
+        'iOS >= 7', 'Android >= 4.1'
+      ]
+    }
+  }
+}
