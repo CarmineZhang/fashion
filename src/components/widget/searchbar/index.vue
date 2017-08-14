c40de7f77f2680d556332ec8decd3a01e4a2097a
zhangmin <157507254@qq.com>
2017-08-14 13:26:25 +0800

    修改searchbar

--- src/components/widget/searchbar/index.vue
+++ src/components/widget/searchbar/index.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="search-bar" id="searchBar" :class="{'search-bar-focusing':isFocus}">
     <form class="search-bar-form">
-      <input type="search" class="search-bar-ipt">
+      <input type="search" class="search-bar-ipt" v-model="searchValue">
     </form>
     <a href="javascript:" class="search-bar-cancel-btn" @click="cancel">取消</a>
   </div>
 </template>
 <script>
+import Debounce from '@/libs/debounce'
 export default {
-  name: 've-searchbar',
+  name: 'search-bar',
   data() {
     return {
       isFocus: false,
       searchValue: ''
+    }
+  },
+  created() {
+    this._debounce = Debounce(() => {
+      this.$emit('on-change', this.searchValue)
+    }, 300)
+  },
+  watch: {
+    searchValue(val, oldVal) {
+      if (val !== '' && val !== oldVal) {
+        this._debounce()
+      }
     }
   },
   methods: {
@@ -32,6 +45,11 @@ export default {
     clear() {
       this.searchValue = ''
       this.isFocus = true
+    }
+  },
+  beforeDestroy() {
+    if (this._debounce) {
+      this._debounce.cancel()
     }
   }
 }
