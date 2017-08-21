60002afc70b3aa8d4c19dd51c5ac7c1bdf718d79
zhangmin <157507254@qq.com>
2017-08-21 16:40:41 +0800

    个人中心

--- src/components/usercenter/myorder.vue
+++ src/components/usercenter/myorder.vue
@@ -6,16 +6,19 @@
     </a>
     <ul class="my-order">
       <li>
-        <a href="">待付款</a>
+        <a href="" class="o-pay">待付款</a>
       </li>
       <li>
-        <a href="">待发货</a>
+        <a href="" class="o-delivery">待发货</a>
       </li>
       <li>
-        <a href="">待收货</a>
+        <a href="" class="o-receive">待收货</a>
       </li>
       <li>
-        <a href="">待评价</a>
+        <a href="" class="o-evaluate">待评价</a>
+      </li>
+      <li>
+        <a href="" class="o-service">售后</a>
       </li>
     </ul>
   </div>
@@ -38,6 +41,43 @@ export default {
       padding: 10px 0;
       color: #666;
       font-size: 12px;
+      &::before {
+        content: '';
+        display: block;
+        width: .42rem;
+        height: .42rem;
+        margin: 7px auto 5px;
+      }
+    }
+    a.o-pay {
+      &:before {
+        background: url('../../assets/order/pay.png') no-repeat;
+        background-size: 100%;
+      }
+    }
+    a.o-delivery {
+      &:before {
+        background: url('../../assets/order/delivery.png') no-repeat;
+        background-size: 100%;
+      }
+    }
+    .o-receive {
+      &:before {
+        background: url('../../assets/order/receive.png') no-repeat;
+        background-size: 100%;
+      }
+    }
+    a.o-evaluate {
+      &:before {
+        background: url('../../assets/order/evaluate.png') no-repeat;
+        background-size: 100%;
+      }
+    }
+    a.o-service {
+      &:before {
+        background: url('../../assets/order/service.png') no-repeat;
+        background-size: 100%;
+      }
     }
   }
 }
--- src/components/usercenter/mytool.vue
+++ src/components/usercenter/mytool.vue
@@ -5,19 +5,22 @@
     </a>
     <ul class="my-tool">
       <li>
-        <a href="">我的积分</a>
+        <a href="" class="t-jifen">积分商品</a>
       </li>
       <li>
-        <a href="">我的好友</a>
+        <a href="" class="t-firend">我的好友</a>
       </li>
       <li>
-        <a href="">我的二维码</a>
+        <a href="" class="t-qrcode">我的二维码</a>
       </li>
       <li>
-        <a href="">购卡</a>
+        <a href="" class="t-buy-card">购卡</a>
       </li>
       <li>
-        <a href="">礼品卡绑定</a>
+        <a href="" class="t-bind-card">礼品卡绑定</a>
+      </li>
+      <li>
+        <a href="" class="t-set">设置</a>
       </li>
     </ul>
   </div>
@@ -49,6 +52,49 @@ export default {
       padding: 10px 0;
       color: #666;
       font-size: 12px;
+      &::before {
+        width: .5rem;
+        height: .5rem;
+        margin: 7px auto 5px;
+        content: '';
+        display: block;
+      }
+    }
+    a.t-jifen {
+      &:before {
+        background: url('../../assets/tools/jfen.png') no-repeat;
+        background-size: 100%;
+      }
+    }
+    a.t-firend {
+      &:before {
+        background: url('../../assets/tools/firend.png') no-repeat;
+        background-size: 100%;
+      }
+    }
+    a.t-qrcode {
+      &:before {
+        background: url('../../assets/tools/qrcode.png') no-repeat;
+        background-size: 100%;
+      }
+    }
+    a.t-buy-card {
+      &:before {
+        background: url('../../assets/tools/buy-card.png') no-repeat;
+        background-size: 100%;
+      }
+    }
+    a.t-bind-card {
+      &:before {
+        background: url('../../assets/tools/card-bind.png') no-repeat;
+        background-size: 100%;
+      }
+    }
+    a.t-set {
+      &:before {
+        background: url('../../assets/tools/set.png') no-repeat;
+        background-size: 100%;
+      }
     }
   }
 }
