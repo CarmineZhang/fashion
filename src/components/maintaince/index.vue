7cb816d3a2b1c2c1f254bc0c9f55600166904707
zhangmin <zhrm1984@163.com>
2017-08-28 21:25:18 +0800

    维修

--- src/components/maintaince/index.vue
+++ src/components/maintaince/index.vue
@@ -10,21 +10,36 @@
         <span v-text="defaultAddr.addressDetail"></span>
       </p>
     </section>
-    <cell title="快递单号">
-      <input type="text" class="ipt">
-    </cell>
-    <cell title="重量">
-      <input type="text" class="ipt">
-    </cell>
+    <cell type="select" title="快递"></cell>
+    <cell-input title="快递单号">
+      <input type="text" class="ipt" v-model="expressNo">
+    </cell-input>
+    <cell-input title="重量">
+      <input type="text" class="ipt" v-model="weight">
+    </cell-input>
+    <div class="div footer-action">
+      <div class="footer-desc">提交后请等待人工审核</div>
+      <a  class="action" @click="submit">提交订单</a>
+    </div>
   </div>
 </template>
 <script>
+import CellInput from '@/components/widget/cell/cellinput'
 import Cell from '@/components/widget/cell'
 import { mapGetters } from 'vuex'
+import * as http from '@/services'
 export default {
   name: 'maintaince',
   components: {
+    CellInput,
     Cell
+  },
+  data(){
+    return {
+      expressNo:'',
+      weight:0,
+      expressName:''
+    }
   },
   computed: {
     ...mapGetters([
@@ -34,6 +49,13 @@ export default {
   methods: {
     selectAddress() {
       this.$router.push({ name: 'order-select-addr' })
+    },
+    submit(){
+      http.afterSaleMaintain().then(res=>{
+        if(res.retcode===0){
+          this.$ve.toast.success('提交成功')
+        }
+      })
     }
   }
 }
---
+++ src/components/widget/cell/cellInput.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="cell cell-form">
+        <div class="cell-hd" v-text="title"></div>
+        <div class="cell-bd">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'cell-input',
+    props: {
+        title: String
+    }
+}
+</script>
+
